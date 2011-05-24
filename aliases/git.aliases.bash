bec78e7baa1ae1e3cdc09db458a4c072d2fe233c
Vincent Wang <linsong.qizi@gmail.com>
2011-05-24 20:42:50 +0800

    add more git alias

--- aliases/git.aliases.bash
+++ aliases/git.aliases.bash
@@ -10,17 +10,31 @@ alias gup='git pull && up.sh'
 alias gdiff='git diff'
 alias gvdiff='git diff | v'
 
+alias gp='git push'
+alias gb='git branch'
+alias gc='git checkout'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gd='git diff'
+alias gs='git status'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull'
+alias gl='git log'
+alias ga='git add'
+alias gcl='git clone'
+
 # following are for git alias
-complete -o bashdefault -o default -o nospace -F _git_checkout gco 2>/dev/null \
-	|| complete -o default -o nospace -F _git_checkout gco
+complete -o bashdefault -o default -o nospace -F _git_checkout gco gc 2>/dev/null \
+	|| complete -o default -o nospace -F _git_checkout gco gc
 complete -o bashdefault -o default -o nospace -F _git_commit gci 2>/dev/null \
 	|| complete -o default -o nospace -F _git_commit gci
-complete -o bashdefault -o default -o nospace -F _git_add gad 2>/dev/null \
-	|| complete -o default -o nospace -F _git_add gad
-complete -o bashdefault -o default -o nospace -F _git_branch gbr 2>/dev/null \
-	|| complete -o default -o nospace -F _git_branch gbr
-complete -o bashdefault -o default -o nospace -F _git_log glg 2>/dev/null \
-	|| complete -o default -o nospace -F _git_log glg
-complete -o bashdefault -o default -o nospace -F _git_diff gdiff 2>/dev/null \
-	|| complete -o default -o nospace -F _git_diff gdiff
+complete -o bashdefault -o default -o nospace -F _git_add gad ga 2>/dev/null \
+	|| complete -o default -o nospace -F _git_add gad ga
+complete -o bashdefault -o default -o nospace -F _git_branch gbr gb 2>/dev/null \
+	|| complete -o default -o nospace -F _git_branch gbr gb
+complete -o bashdefault -o default -o nospace -F _git_log glg gl 2>/dev/null \
+	|| complete -o default -o nospace -F _git_log glg gl
+complete -o bashdefault -o default -o nospace -F _git_diff gdiff gd 2>/dev/null \
+	|| complete -o default -o nospace -F _git_diff gdiff gd
 
